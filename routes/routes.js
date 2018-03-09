be50705428fd31ff1b84b7328e1c1ba3e7e9e2b6
JeromeDavison <36089347+JeromeDavison@users.noreply.github.com>
2018-03-09 12:05:35 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,14 @@
+var passportLocalMongoose = require('passport-local-mongoose');
+var passport = require('passport');
+
 const dbs = require('../database/dbs');
+
+
+// use static authenticate method of model in LocalStrategy
+passport.use(Users.User.createStrategy());
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(dbs.User.serializeUser());
+passport.deserializeUser(dbs.User.deserializeUser());
 
 
 
@@ -9,9 +19,56 @@ module.exports = function (app){
 
     
 });
+app.post('/register', (req, res) => { //passport local registration 
+dbs.User.register({username:req.body.username}, req.body.password, (err, succ) => {
+	if (err){
+		res.json({data:"Sorry, couldnt be registered!"});
+	} else {
+	passport.authenticate('local')(req,res, () => {
+	res.json({data:"You've been signed up!"});
+	});
+	}
+ });
+});
+
+app.post('/login', authenticate('local'), (req, res) => { //authentication for regular login 
+    res.sendFile(__dirname+ '/src/index.html');
+
+});
+
+
+
+
 	// listUsersInGoal
 	app.get('/listUsersInGoal', (req, res) => {
-	var groupList = [];
+		
+	/* let listUsers(){ put this in mod folder
+		return new Promise((resolve, reject) =>{
+			dbs.groups.find({}, (err, succ) => {
+		if (err){
+			reject("sorry, couldnt find the group");	
+		} else {
+			for (x = 0; x < succ.length; x++){
+				groupList.push(succ[x].user);
+			}
+			resolve(groupList);
+		}
+		
+		
+		
+		
+		
+	});
+			
+			
+		});
+		
+	}
+		*/
+		
+		
+		
+	var groupList = []; // list everyone in a group 
     dbs.groups.find({}, (err, succ) => {
 		if (err){
 			res.json({data: "Sorry, could not reteive users!"});	
@@ -49,27 +106,58 @@ module.exports = function (app){
 				newGroup.user = req.user.username;
 				newGroup.goal = req.body.goal;
 				newGroup.save().then(function(saved){
-					res.json({data:"You've saved!"
-					})
+					res.json({data:"You've saved!"})
 					
 					
 				})
 				.catch(function(lost){
 					res.json({data:"Sorry, there was an error in saving!"})
-				})
-				
-				
-			}
-			
-			
-			
-		})
-    
+				})	
+		}	
+	})
 });
 	
 	// delete goal
 	app.post('/leaveGoal', (req, res) => {
-    
+    dbs.Group.find({goalId:req.body.id}, (err, succ) =>
+	 if (err){
+		 res.json({data:"sorry, there was no goal matching the id"});
+		 
+	 } else {
+		 succ[0].remove((fail, win) => {
+		 if (fail){
+			 res.json({data:"wasnt able to remove"});
+		 }
+		 else {
+			dbs.count.find({user:req.body.user}, (err1, succ1) => {
+				if (succ1){
+					succ1[0].count = succ1[0].count - 1;
+					succ1[0].save().then(function(save){
+					res.json({data:"congrats! youve left the goal!", info:save}); 
+
+						
+						
+					});
+
+				}
+				
+			});
+			 
+		 }
+		 
+		 
+		 
+		 
+		 });
+		
+	 }
+	
+	
+	
+	
+	
+	
+	});
 });
 	
 	
@@ -118,4 +206,3 @@ module.exports = function (app){
 	
 	
 }
-
